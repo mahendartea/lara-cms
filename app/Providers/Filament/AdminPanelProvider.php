a87f5f64fada4b9ce52b9e69d10653cb99e72f19
Mahendar Dwi Payana <38557290+mahendartea@users.noreply.github.com>
2024-11-01 15:20:01 +0700

    Update Logo dan Nama

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,11 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandLogo(asset('/assets/img/LogoPPA.png'))
+            ->brandLogoHeight('80px')
+            ->brandName('Partai Perjuangan Aceh')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => '#040181',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
